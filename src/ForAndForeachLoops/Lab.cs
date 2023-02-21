ade157930c2812f9d94ba1c6af5beb6caa145e53
enzotlucas <enzotlucas31@gmail.com>
2023-02-21 17:02:47 -0300

    feat: adding string concat

--- src/ForAndForeachLoops/Lab.cs
+++ src/ForAndForeachLoops/Lab.cs
@@ -8,20 +8,20 @@ namespace ForAndForeachLoops
         [Params(10, 100, 100000)]
         public int Number;
 
-        private List<int> NumberList;
-        private int[] NumberArray;
+        private List<int> _numberList;
+        private int[] _numberArray;
 
         [GlobalSetup]
         public void Setup()
         {
-            NumberList = Enumerable.Range(1, Number).ToList();
-            NumberArray = Enumerable.Range(1, Number).ToArray();
+            _numberList = Enumerable.Range(1, Number).ToList();
+            _numberArray = Enumerable.Range(1, Number).ToArray();
         }
 
         [Benchmark]
         public void ForeachLoopWithList()
         {
-            foreach (var number in NumberList)
+            foreach (var number in _numberList)
             {
                 var test = number * number;
                 test -= number;
@@ -32,7 +32,7 @@ namespace ForAndForeachLoops
         [Benchmark]
         public void ForeachLoopWithArray()
         {
-            foreach (var number in NumberArray)
+            foreach (var number in _numberArray)
             {
                 var test = number * number;
                 test -= number;
@@ -43,9 +43,9 @@ namespace ForAndForeachLoops
         [Benchmark]
         public void ForLoopWithList()
         {
-            for (int i = 0; i < NumberList.Count; i++)
+            for (int i = 0; i < _numberList.Count; i++)
             {
-                var number = NumberList[i];
+                var number = _numberList[i];
                 var test = number * number;
                 test -= number;
                 test.ToString();
@@ -55,9 +55,9 @@ namespace ForAndForeachLoops
         [Benchmark]
         public void ForLoopWithArray()
         {
-            for (int i = 0; i < NumberArray.Length; i++)
+            for (int i = 0; i < _numberArray.Length; i++)
             {
-                var number = NumberArray[i];
+                var number = _numberArray[i];
                 var test = number * number;
                 test -= number;
                 test.ToString();
@@ -67,7 +67,7 @@ namespace ForAndForeachLoops
         [Benchmark]
         public void ParallelForeachLoopWithList()
         {
-            Parallel.ForEach(NumberList, number =>
+            Parallel.ForEach(_numberList, number =>
             {
                 var test = number * number;
                 test -= number;
@@ -78,7 +78,7 @@ namespace ForAndForeachLoops
         [Benchmark]
         public void ParallelForeachLoopWithArray()
         {
-            Parallel.ForEach(NumberArray, number =>
+            Parallel.ForEach(_numberArray, number =>
             {
                 var test = number * number;
                 test -= number;
@@ -89,9 +89,9 @@ namespace ForAndForeachLoops
         [Benchmark]
         public void ParallelForLoopWithList()
         {
-            Parallel.For(0, NumberList.Count, i =>
+            Parallel.For(0, _numberList.Count, i =>
             {
-                var number = NumberList[i];
+                var number = _numberList[i];
                 var test = number * number;
                 test -= number;
                 test.ToString();
@@ -101,9 +101,9 @@ namespace ForAndForeachLoops
         [Benchmark]
         public void ParallelForLoopWithArray()
         {
-            Parallel.For(0, NumberArray.Length, i =>
+            Parallel.For(0, _numberArray.Length, i =>
             {
-                var number = NumberArray[i];
+                var number = _numberArray[i];
                 var test = number * number;
                 test -= number;
                 test.ToString();
---
+++ src/StringConcat/Lab.cs
@@ -0,0 +1,237 @@
+﻿using BenchmarkDotNet.Attributes;
+using System.Text;
+
+namespace StringConcat
+{
+    [MemoryDiagnoser]
+    public class Lab
+    {
+        [Params(10, 100)]
+        public int Number;
+
+        private string[] _words;
+        private readonly string _phrase = "Lorem ipsum dolor sit amet.";
+        private readonly string _concatPhrase = "Sed recusandae maiores aut ipsa quidem aut illum";
+        private readonly string _concatWord = "Lorem";
+
+        [GlobalSetup]
+        public void Setup()
+        {
+            _words = new string[Number]; 
+            Array.Fill(_words, _concatWord);
+        }
+
+
+
+        //Operator
+        [Benchmark]
+        public void ConcatUsingOperator()
+        {
+            var result = _phrase + " " + _concatPhrase;
+
+            Console.WriteLine(result);
+        }
+
+        [Benchmark]
+        public void ConcatCollectionUsingOperatorAndForeach()
+        {
+            var result = _phrase;
+
+            foreach (var word in _words)
+            {
+                result = result + " " + word;
+            }
+
+            Console.WriteLine(result);
+        }
+
+        [Benchmark]
+        public void ConcatCollectionUsingOperatorAndFor()
+        {
+            var result = _phrase;
+
+            for (int i = 0; i < Number; i++)
+            {
+                var word = _words[i];
+
+                result = result + " " + word;
+            }
+
+            Console.WriteLine(result);
+        }
+
+
+
+        //Interpolation
+        [Benchmark]
+        public void ConcatUsingInterpolation()
+        {
+            var result = $"{_phrase} {_concatPhrase}";
+
+            Console.WriteLine(result);
+        }
+
+        [Benchmark]
+        public void ConcatCollectionUsingInterpolationAndForeach()
+        {
+            var result = _phrase;
+
+            foreach (var word in _words)
+            {
+                result = $"{result} {word}";
+            }
+
+            Console.WriteLine(result);
+        }
+
+        [Benchmark]
+        public void ConcatCollectionUsingInterpolationAndFor()
+        {
+            var result = _phrase;
+
+            for (int i = 0; i < Number; i++)
+            {
+                var word = _words[i];
+
+                result = $"{result} {word}";
+            }
+
+            Console.WriteLine(result);
+        }
+
+
+
+        //StringFormat
+        [Benchmark]
+        public void ConcatUsingStringFormat()
+        {
+            var result = string.Format("{0} {1}", _phrase, _concatPhrase);
+
+            Console.WriteLine(result);
+        }
+
+        [Benchmark]
+        public void ConcatCollectionUsingStringFormatAndForeach()
+        {
+            var result = _phrase;
+
+            foreach (var word in _words)
+            {
+                result = string.Format("{0} {1}", result, word);
+            }
+
+            Console.WriteLine(result);
+        }
+
+        [Benchmark]
+        public void ConcatCollectionUsingStringFormatAndFor()
+        {
+            var result = _phrase;
+
+            for (int i = 0; i < Number; i++)
+            {
+                var word = _words[i];
+
+                result = string.Format("{0} {1}", result, word);
+            }
+
+            Console.WriteLine(result);
+        }
+
+
+
+        //StringBuilder
+        [Benchmark]
+        public void ConcatUsingStringBuilder()
+        {
+            var builder = new StringBuilder();
+
+            builder.Append(_phrase);
+            builder.Append(' ');
+            builder.Append(_concatPhrase);
+
+            var result = builder.ToString();
+
+            Console.WriteLine(result);
+        }
+
+        [Benchmark]
+        public void ConcatCollectionUsingStringBuilderAndForeach()
+        {
+            var builder = new StringBuilder();
+
+            builder.Append(_phrase);
+
+            foreach (var word in _words)
+            {
+                builder.Append(' ');
+
+                builder.Append(word);
+            }
+
+            var result = builder.ToString();
+
+            Console.WriteLine(result);
+        }
+
+        [Benchmark]
+        public void ConcatCollectionUsingStringBuilderAndFor()
+        {
+            var builder = new StringBuilder();
+
+            builder.Append(_phrase);
+
+            for (int i = 0; i < Number; i++)
+            {
+                var word = _words[i];
+
+                builder.Append(' ');
+
+                builder.Append(word);
+            }
+
+            var result = builder.ToString();
+
+            Console.WriteLine(result);
+        }
+
+
+
+        //StringConcat
+        [Benchmark]
+        public void ConcatCollectionUsingStringConcat()
+        {
+            var result = _phrase;
+
+            result += string.Concat(" ", _words);
+
+            Console.WriteLine(result);
+        }
+
+
+
+        //StringJoin
+        [Benchmark]
+        public void ConcatCollectionUsingStringJoin()
+        {
+            var result = _phrase;
+
+            result += string.Join(" ", _words);
+
+            Console.WriteLine(result);
+        }
+
+
+
+        //LINQ
+        [Benchmark]
+        public void ConcatCollectionUsingLinqAggregate()
+        {
+            var result = _phrase;
+
+            result = _words.Aggregate((partialPhrase, word) => $"{partialPhrase} {word}");
+
+            Console.WriteLine(result);
+        }
+    }
+}
